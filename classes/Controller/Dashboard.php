c22de6d9dd214ec4bb1a4a5efb469336990e250e
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-10-24 16:46:31 +0200

    Adds auth

--- classes/Controller/Dashboard.php
+++ classes/Controller/Dashboard.php
@@ -11,8 +11,50 @@
 
 namespace Indigo\Admin\Controller;
 
+use Fuel\Auth\Manager;
+use Fuel\Auth\Storage;
+use Fuel\Auth\Persistence;
+use Fuel\Auth\User;
+use Fuel\Auth\Group;
+use Fuel\Auth\Role;
+use Fuel\Auth\Acl;
+
 class Dashboard extends \Fuel\Controller\Base
 {
+	/**
+	 * Auth Manager
+	 *
+	 * @var Manager
+	 */
+	protected $auth;
+
+	protected function getAuth()
+	{
+		if ( ! $this->auth)
+		{
+			$this->auth = new Manager(
+				new Storage\File(['file' => '/tmp']),
+				new Persistence\File(['file' => '/tmp'])
+			);
+
+			// assign our Auth drivers
+			$this->auth->addDriver(new User\File(['min_password_length' => 6, 'new_password_length' => 8, 'file' => '/tmp']), 'user');
+			$this->auth->addDriver(new Group\File(['file' => '/tmp']), 'group');
+			$this->auth->addDriver(new Role\File(['file' => '/tmp']), 'role');
+			$this->auth->addDriver(new Acl\File(['file' => '/tmp']), 'acl');
+		}
+
+		return $this->auth;
+	}
+
+	public function before()
+	{
+		$manager = $this->getAuth();
+
+		if ($manager->check()) {
+		}
+	}
+
 	/**
 	 * The basic welcome message
 	 *
